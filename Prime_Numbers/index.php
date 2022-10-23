d809461f8cfe419f7d5f8dfd04758828cad86899
sm@xisnet.pt <1060371@isep.ipp.pt>
2022-10-23 01:26:07 +0100

    Prime_Numbers - First Draft

---
+++ Prime_Numbers/index.php
@@ -0,0 +1,24 @@
+<?php 
+    for($i=1;$i<=100;$i++){
+        $primeNumb = array();
+        echo "$i [";
+        for ($j=$i; $j>1; $j--)
+        {
+            if($i % $j == 0){
+                array_push($primeNumb,$j);
+            }
+        }
+
+        if(sizeof($primeNumb)>1)
+        {
+            for($j=0;$j<sizeof($primeNumb);$j++){
+                echo $primeNumb[$j];
+                if($j < sizeof($primeNumb)-1)  
+                    echo ',';
+            }
+            echo '] <br>';
+        }
+        else echo "PRIME] <br>"; 
+
+    }
+?>
