89ba2ee14a4c81144e51f4b9e0a141f87f2e3c3c
sm@xisnet.pt <1060371@isep.ipp.pt>
2022-11-04 21:56:54 +0000

    Prime Numbers - Added PSR-12 & Readme

---
+++ Prime_Numbers/README.md
@@ -0,0 +1,8 @@
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://media-exp1.licdn.com/dms/image/C4D1BAQG2FLIXvKlHXQ/company-background_10000/0/1610972636996?e=1668189600&v=beta&t=U-Veb2PvgN1rpYBesvPid8q7PB7tbyV0B5CDxZms0HY" width="400" alt="EXADS Logo"></a></p>
+
+
+## 1. Prime Numbers
+
+Write a PHP script that prints all integer values from 1 to 100.
+Beside each number, print the numbers it is a multiple of (inside brackets and comma-separated). 
+If only multiple of itself then print “[PRIME]”.
--- Prime_Numbers/index.php
+++ Prime_Numbers/index.php
@@ -1,24 +1,23 @@
-<?php 
-    for($i=1;$i<=100;$i++){
-        $primeNumb = array();
-        echo "$i [";
-        for ($j=$i; $j>1; $j--)
-        {
-            if($i % $j == 0){
-                array_push($primeNumb,$j);
-            }
-        }
+<?php
 
-        if(sizeof($primeNumb)>1)
-        {
-            for($j=0;$j<sizeof($primeNumb);$j++){
-                echo $primeNumb[$j];
-                if($j < sizeof($primeNumb)-1)  
-                    echo ',';
-            }
-            echo '] <br>';
+for ($i = 1; $i <= 100; $i++) {
+    $primeNumb = array();
+    echo "$i [";
+    for ($j = $i; $j > 1; $j--) {
+        if ($i % $j == 0) {
+            array_push($primeNumb, $j);
         }
-        else echo "PRIME] <br>"; 
-
     }
-?>
+
+    if (sizeof($primeNumb) > 1) {
+        for ($j = 0; $j < sizeof($primeNumb); $j++) {
+            echo $primeNumb[$j];
+            if ($j < sizeof($primeNumb) - 1) {
+                echo ',';
+            }
+        }
+        echo '] <br>';
+    } else {
+        echo "PRIME] <br>";
+    }
+}
